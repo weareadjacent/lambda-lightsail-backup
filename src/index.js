7ddae6a0c865175385fb631e782119b6899fbd32
Tom McCaffery <tom@weareadjacent.com>
2018-06-26 00:17:52 -0400

    Get backups sorted by instance

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const lightsail = new AWS.Lightsail();
 
 const BACKUP_INSTANCES = process.env.BACKUP_INSTANCES.split(',');
 let allSnapshots;
-let allSnapshotsByInstance = [];
+let backupsByInstance = {};
 
 exports.handler = async (event, context, callback) => {
   for (let instance of BACKUP_INSTANCES) {
@@ -19,15 +19,25 @@ exports.handler = async (event, context, callback) => {
 async function getBackups(instanceName) {
   if (!allSnapshots) {
     allSnapshots = (await lightsail.getInstanceSnapshots().promise()).instanceSnapshots;
+
     for (let snapshot of allSnapshots) {
       let instance = snapshot.fromInstanceName;
-      if (!allSnapshotsByInstance[instance]) {
-        allSnapshotsByInstance[instance] = [];
+      if (!backupsByInstance[instance]) {
+        backupsByInstance[instance] = [];
       }
 
-      allSnapshotsByInstance[instance].push(snapshot);
+      backupsByInstance[instance].push(snapshot);
+    }
+
+    // Sort by date
+    for (let [instance, backups] of Object.entries(backupsByInstance)) {
+      backups.sort(dateSort);
     }
   }
 
-  return allSnapshotsByInstance[instanceName];
+  return backupsByInstance[instanceName];
+}
+
+function dateSort(a, b){
+  return new Date(a.createdAt) - new Date(b.createdAt);
 }
