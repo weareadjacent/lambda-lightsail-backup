a9ffaa74625fce5d991de40fb14d832c3330f24d
Tom McCaffery <tom@weareadjacent.com>
2018-06-26 10:11:51 -0400

    Untested refactoring

--- src/index.js
+++ src/index.js
@@ -5,56 +5,93 @@ AWS.config.update({region: 'us-east-1'});
 
 const lightsail = new AWS.Lightsail();
 
+/**
+ * List of target instances from environment variables.
+ * @type {Array}
+ */
 const BACKUP_INSTANCES = process.env.BACKUP_INSTANCES.split(',');
 
-let allSnapshots;
-let backupsByInstance = {};
+/**
+ * Convenience for calculating dates
+ * @type {Date}
+ */
+const NOW = new Date();
+const NOW_DATE_STRING = NOW.toDateString();
 
+/**
+ * Store backups chronologically, keyed by instance name.
+ * @type {Object}
+ */
+let backups = {};
+
+
+/**
+ * Main handler from AWS lambda.
+ */
 exports.handler = (event, context, callback) => {
   (async function(){
+    await loadBackups();
+
     for (let instance of BACKUP_INSTANCES) {
-      let latest = await getLatestBackup(instance);
-  
-      if (latest.createdAt.toDateString() !== (new Date()).toDateString()) {
+      if (!hasBackupToday(instance)) {
         createBackup(instance);
       }
+
+      pruneBackups(instance);
     }
   })();
 };
 
-async function getBackups(instance) {
-  if (!allSnapshots) {
-    allSnapshots = (await lightsail.getInstanceSnapshots().promise()).instanceSnapshots;
+async function loadBackups() {
+  // TODO handle paging
+  let snapshots = (await lightsail.getInstanceSnapshots().promise()).instanceSnapshots;
 
-    for (let snapshot of allSnapshots) {
-      let instance = snapshot.fromInstanceName;
-      if (!backupsByInstance[instance]) {
-        backupsByInstance[instance] = [];
-      }
+  for (let snapshot of snapshots) {
+    // Ignore snapshots not created via this script.
+    if (!isBackupSnapshot(snapshot)) continue;
 
-      backupsByInstance[instance].push(snapshot);
-    }
+    let instance = snapshot.fromInstanceName;
 
-    // Sort by date
-    for (let [instance, backups] of Object.entries(backupsByInstance)) {
-      backups.sort(dateSort);
-    }
+    // Ignore other instances.
+    if (!shouldBackupInstance(instance)) continue;
+
+    // Create an array per instance.
+    backups[instance] = backups[instance] || [];
+    backups[instance].push(snapshot);
   }
 
-  return backupsByInstance[instance];
+  // Sort by date.
+  for (let [instance, backups] of Object.entries(backups)) {
+    backups.sort((a, b) => a.createdAt - b.createdAt);
+  }
 }
 
-async function getLatestBackup(instance) {
-  let backups = await getBackups(instance);
-  return backups[backups.length - 1];
+/**
+ * Whether or not a snapshot was created by this script (looks at name).
+ * @param  {Object}   snapshot Snapshot object
+ * @return {Boolean}
+ */
+function isBackupSnapshot(snapshot) {
+  return true;
+  // return snapshot.name.endsWith('-autosnap');
 }
 
-function dateSort(a, b){
-  return a.createdAt - b.createdAt;
+function shouldBackupInstance(instance) {
+  return BACKUP_INSTANCES.includes(instance);
+}
+
+function getLatestBackup(instance) {
+  return backups[instance][backups[instance]length - 1];
+}
+
+function hasBackupToday(instance) {
+  console.log(`${instance}: Checking for today's backup`);
+  let latest = getLatestBackup(instance);
+  return latest.createdAt.toDateString() === NOW_DATE_STRING;
 }
 
 function createBackup(instance) {
-  console.log(`Creating backup of ${instance}`);
+  console.log(`${instance}: Creating backup`);
 
   let date = new Date();
   let name = `${instance}-${date.getTime()}-autosnap`;
@@ -65,9 +102,13 @@ function createBackup(instance) {
 
   lightsail.createInstanceSnapshot(params, function(err, data) {
     if (err) {
-      console.error(err);
+      console.error(`${instance}: Error creating snapshot`, err);
     } else {
-      console.log(data);
+      console.log(`${instance}: Snapshot created`, data);
     }
   });
+}
+
+function pruneBackups(instance) {
+  console.log(`${instance}: Pruning backups`);
 }
