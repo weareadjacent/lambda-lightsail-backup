0cae8c206629384b1ca487d7447ac3e87052f4e0
Tom McCaffery <tom@weareadjacent.com>
2018-06-26 00:02:17 -0400

    Get backups by instance

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 'use strict';
 
-exports.handler = (event, context, callback) => {
-    console.log(process.env.BACKUP_INSTANCES);
-    const BACKUP_INSTANCES = process.env.BACKUP_INSTANCES.split(',');
-    console.log(BACKUP_INSTANCES);
-    
-    const AWS = require('aws-sdk');
-    AWS.config.update({ region: 'us-east-1' });
-    console.log(AWS.config);
+const AWS = require('aws-sdk');
+AWS.config.setPromisesDependency(null);
+AWS.config.update({ region: 'us-east-1' });
+const lightsail = new AWS.Lightsail();
 
-    const lightsail = new AWS.Lightsail();
+const BACKUP_INSTANCES = process.env.BACKUP_INSTANCES.split(',');
+let allSnapshots;
+let allSnapshotsByInstance = [];
+
+exports.handler = async (event, context, callback) => {
+  for (let instance of BACKUP_INSTANCES) {
+    let snapshots = await getBackups(instance);
+    console.log(snapshots);
+  }
 };
+
+async function getBackups(instanceName) {
+  if (!allSnapshots) {
+    allSnapshots = (await lightsail.getInstanceSnapshots().promise()).instanceSnapshots;
+    for (let snapshot of allSnapshots) {
+      let instance = snapshot.fromInstanceName;
+      if (!allSnapshotsByInstance[instance]) {
+        allSnapshotsByInstance[instance] = [];
+      }
+
+      allSnapshotsByInstance[instance].push(snapshot);
+    }
+  }
+
+  return allSnapshotsByInstance[instanceName];
+}
